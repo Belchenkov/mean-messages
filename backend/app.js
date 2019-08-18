a3b392f64ced8f1d77741e83e329d5b5fea64dca
Aleksey Belchenkov <u608110@gmail.com>
2019-08-18 11:37:14 +0300

    Adding Post on Server

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -31,6 +35,14 @@ app.use('/api/posts', (req, res, next) => {
   return res.status(200).json({
     message: 'Success',
     posts
+  });
+});
+
+app.post('/api/posts', (req, res, next) => {
+  const { post } = req.body;
+  console.log(post);
+  return res.status(201).json({
+    message: 'Success'
   });
 });
 
