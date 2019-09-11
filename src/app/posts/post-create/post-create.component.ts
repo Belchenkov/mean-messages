774da7165d61a68daffc26579e8fdc370a989989
Aleksey Belchenkov <u608110@gmail.com>
2019-09-11 07:40:31 +0300

    Creating the edit Form

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -3,8 +3,10 @@ import {
   OnInit
 } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 
 import { PostsService } from '../posts.service';
+import { Post } from '../post.model';
 
 @Component({
   selector: 'app-post-create',
@@ -12,7 +14,11 @@ import { PostsService } from '../posts.service';
   styleUrls: ['./post-create.component.css']
 })
 export class PostCreateComponent implements OnInit {
-  constructor(public postsService: PostsService) {}
+  private mode = 'create';
+  private postId: string;
+  private post: Post;
+
+  constructor(public postsService: PostsService, public route: ActivatedRoute) {}
 
   onAddPost(form: NgForm) {
     if (form.invalid) {
@@ -23,6 +29,16 @@ export class PostCreateComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.route.paramMap.subscribe((paramMap: ParamMap) => {
+      if (paramMap.has('postId')) {
+        this.mode = 'edit';
+        this.postId = paramMap.get('postId');
+        this.post = this.postsService.getPost(this.postId);
+      } else {
+        this.mode = 'create';
+        this.postId = null;
+      }
+    });
   }
 
 }
