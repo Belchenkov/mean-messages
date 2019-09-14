a79ca0c54805d83c5580e7ead0cef167c3f684cd
Aleksey Belchenkov <u608110@gmail.com>
2019-09-14 06:37:55 +0300

    Re-Organizing Backend Routes

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+const postsRoutes = require('./routes/posts');
+
 mongoose.connect('mongodb://belchenkov:12qwasZX@ds217078.mlab.com:17078/mean_messages',
   { useNewUrlParser: true }
 )
@@ -14,7 +16,6 @@ mongoose.connect('mongodb://belchenkov:12qwasZX@ds217078.mlab.com:17078/mean_mes
     console.log(err);
   });
 
-const Post = require('./models/post');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -32,66 +33,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get('/api/posts', (req, res, next) => {
-  Post.find()
-    .then(docs => {
-      return res.status(200).json({
-        message: 'Posts fetched successfully',
-        posts: docs
-      });
-    });
-});
-
-app.post('/api/posts', (req, res, next) => {
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content
-  });
-
-  post.save()
-    .then(createdPost => {
-      res.status(201).json({
-        message: 'Post added successfully',
-        postId: createdPost.id
-      });
-    });
-});
-app.put('/api/posts/:id', (req, res, next) => {
-  const post = new Post({
-    _id: req.body.id,
-    title: req.body.title,
-    content: req.body.content
-  });
-
-  Post.updateOne({ _id: req.params.id }, post)
-    .then(result => {
-      res.status(200).json({message: 'Update successful'});
-    })
-    .catch(err => console.log(err));
-});
-
-app.get('/api/posts/:id', (req, res, next) => {
-  Post.findById(req.params.id)
-    .then(post => {
-      if (post) {
-        res.status(200).json(post);
-      } else {
-        res.status(404).json({message: 'Post not found!'});
-      }
-    })
-    .catch(err => {
-      console.log(err);
-    });
-});
-
-app.delete('/api/posts/:id', (req, res, next) => {
-  Post.deleteOne({_id: req.params.id})
-    .then(result => {
-      console.log(result);
-      res.status(200).json({
-        message: 'Post deleted!'
-      });
-    });
-});
+app.use('/api/posts', postsRoutes);
 
 module.exports = app;
