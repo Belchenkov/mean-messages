a91198e0a2d989d50ae7edbaa30da95e805cf090
Aleksey Belchenkov <u608110@gmail.com>
2019-09-18 07:21:18 +0300

    Adding Image Controls to Store the Image

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -47,7 +47,10 @@ export class PostCreateComponent implements OnInit {
       content: new FormControl(null,
         {
           validators: [Validators.required]
-        })
+        }),
+      image: new FormControl(null, {
+        validators: [Validators.required]
+      })
     });
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
       if (paramMap.has('postId')) {
@@ -72,6 +75,13 @@ export class PostCreateComponent implements OnInit {
         this.postId = null;
       }
     });
+  }
+
+  onImagePicked(event: Event) {
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({image: file});
+    this.form.get('image').updateValueAndValidity();
+
   }
 
 }
