5e4826432ba86502c3f6d0388c0be9c3ae41dced
Aleksey Belchenkov <u608110@gmail.com>
2019-10-14 07:59:55 +0300

    Sending the token to the frontend

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,10 @@
-const express = require('express');
-const bcrypt = require('bcrypt');
-const router = express.Router();
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-const User = require('../models/user');
+const User = require("../models/user");
+
+const router = express.Router();;
 
 router.post('/signup', (req, res, next) => {
   bcrypt.hash(req.body.password, 10)
@@ -25,31 +26,34 @@ router.post('/signup', (req, res, next) => {
     });
 });
 
-router.post('/login', (req, res, next) => {
+router.post("/login", (req, res, next) => {
+  let fetchedUser;
   User.findOne({ email: req.body.email })
     .then(user => {
       if (!user) {
         return res.status(401).json({
-          message: 'Auth failed!'
+          message: "Auth failed"
         });
       }
+      fetchedUser = user;
       return bcrypt.compare(req.body.password, user.password);
     })
     .then(result => {
       if (!result) {
         return res.status(401).json({
-          message: 'Auth failed!'
+          message: "Auth failed"
         });
       }
       const token = jwt.sign(
-        { email: user.email, userId: user._id },
-        'secret_this_should_be_longer',
-        { expiresIn: '1h' }
+        { email: fetchedUser.email, userId: fetchedUser._id },
+        "secret_this_should_be_longer",
+        { expiresIn: "1h" }
       );
+      res.status(200).json({ token });
     })
     .catch(err => {
       return res.status(401).json({
-        message: 'Auth failed!'
+        message: "Auth failed"
       });
     });
 });
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+import { AuthService } from '../auth.service';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -15,7 +17,10 @@ export class LoginComponent implements OnInit {
   }
 
   onLogin(form: NgForm) {
-
+    if (form.invalid) {
+      return;
+    }
+    this.authService.login(form.value.email, form.value.password);
   }
 
 }
