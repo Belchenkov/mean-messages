bfbcad7691b17f9f24feea0af7674ae3ab4a9dec
Aleksey Belchenkov <u608110@gmail.com>
2019-11-12 06:35:23 +0300

    Adding an Error Dialog

--- src/app/error-interceptor.ts
+++ src/app/error-interceptor.ts
@@ -20,7 +20,13 @@ export class ErrorInterceptor implements HttpInterceptor {
     return next.handle(req)
       .pipe(
         catchError((error: HttpErrorResponse) => {
-          this.dialog.open(ErrorComponent);
+          let errorMessage = 'An unknown error occurred!';
+
+          if (error.error.message) {
+            errorMessage = error.error.message;
+          }
+
+          this.dialog.open(ErrorComponent, {data: {message: errorMessage}});
           return throwError(error);
         })
       );
