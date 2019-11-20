a2e47c3777fe13a75bd6c251fe0752e206eb4555
Aleksey Belchenkov <u608110@gmail.com>
2019-11-20 07:44:25 +0300

    Separating the Middleware

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,51 +1,24 @@
 const express = require('express');
 const router = express.Router();
-const multer = require('multer');
-
-const PostController = require('../controllers/posts');
 
 const checkAuth = require('../middleware/check-auth');
+const extractFile = require('../middleware/file');
 
-const MIME_TYPE_MAP = {
-  'image/png': 'png',
-  'image/jpeg': 'jpg',
-  'image/jpg': 'jpg'
-};
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    const isValid = MIME_TYPE_MAP[file.mimetype];
-    let error = new Error('Invalid mime type');
-
-    if (isValid) {
-      error = null;
-    }
-
-    cb(null, 'backend/images');
-  },
-  filename: (req, file, cb) => {
-    const name = file.originalname
-      .toLocaleLowerCase()
-      .split(' ')
-      .join('-');
-    const ext = MIME_TYPE_MAP[file.mimetype];
-    cb(null, name + '-' + Date.now() + '.' + ext);
-  }
-});
+const PostController = require('../controllers/posts');
 
 router.get('/', PostController.getPosts);
 
 router.post(
   '/',
   checkAuth,
-  multer({storage}).single('image'),
+  extractFile,
   PostController.createPost
   );
 
 router.put(
   '/:id',
   checkAuth,
-  multer({storage}).single('image'),
+  extractFile,
   PostController.updatePost
   );
 
